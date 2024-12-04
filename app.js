bfe03bbc6dbc0e89d4439970aa489f46e3e3822e
JoannaTalitod2005 <joanna.talitod@gmail.com>
2024-12-04 07:40:13 +0000

    added images for projects

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 (function () {
-    emailjs.init("5OAIPyAkkCFbj8W6k");
-  })();
-  
-  document
-    .getElementById("contact-form")
-    .addEventListener("submit", function (event) {
-      event.preventDefault();
-  
-      emailjs.sendForm("service_fce1boh", "template_6qjtyp8", this).then(
-        function () {
-          alert("Message Sent Successfully!");
-        },
-  
-        function (error) {
-          alert("Failed to send the message: " + JSON.stringify(error));
-        }
-      );
-    });
+  emailjs.init("5OAIPyAkkCFbj8W6k");  
+})();
+
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevent the default form submission
+
+  emailjs.sendForm("service_fce1boh", "template_6qjtyp8", this).then(
+    function () {
+      document.querySelector(".response").style.display = "block";
+      document.querySelector(".response").textContent = "Thank you for your message!";
+      setTimeout(() => {
+        document.querySelector(".response").style.display = "none";
+      }, 3000);
+    },
+    function (error) {
+      alert("Failed to send the message: " + JSON.stringify(error));
+    }
+  );
+});
